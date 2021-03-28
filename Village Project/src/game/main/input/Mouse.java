cc9430855871aea3841291d35458f0885827314d
YouLikeCats <thisisbryanchan@gmail.com>
2021-03-28 01:18:01 -0400

    Mouse Changes

--- Village Project/src/game/main/input/Mouse.java
+++ Village Project/src/game/main/input/Mouse.java
@@ -10,39 +10,39 @@ public class Mouse implements MouseListener, MouseMotionListener { //ok that's 2
 	public int mb = -1; //gives us the mouse button that's being pressed
 	
 	@Override
-	public void mouseClicked(MouseEvent e) {
+	public void mouseClicked(MouseEvent e) { //ew no one cares
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {
-		mb = e.getButton();
+	public void mousePressed(MouseEvent e) { //WHEN A MOUSE BUTTON IS PRESSED
+		mb = e.getButton(); //set the mouse button variable to the value of that mouse button
 	}
 
 	@Override
-	public void mouseReleased(MouseEvent e) {
-		mb = -1;
+	public void mouseReleased(MouseEvent e) { //WHEN A MOUSE BUTTON IS RELEASED
+		mb = -1; //change it back to default to signify that it's no longer being pressed
 	}
 
 	@Override
-	public void mouseEntered(MouseEvent e) {
+	public void mouseEntered(MouseEvent e) { //ew no one cares
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void mouseExited(MouseEvent e) {
+	public void mouseExited(MouseEvent e) { //ew no one cares
 		// TODO Auto-generated method stub
 	}
 
 	@Override
-	public void mouseDragged(MouseEvent e) {
-		// TODO Auto-generated method stub
+	public void mouseDragged(MouseEvent e) { //ew no one cares
+		// TODO Auto-generated method stub 
 	}
 
 	@Override
-	public void mouseMoved(MouseEvent e) {
-		x = e.getX();
-		y = e.getY();
+	public void mouseMoved(MouseEvent e) { //WHEN THE MOUSE IS MOVED
+		x = e.getX(); //set x to the mouse's x position
+		y = e.getY(); //set y to the mouse's y posiiton
 	}
 
 }
